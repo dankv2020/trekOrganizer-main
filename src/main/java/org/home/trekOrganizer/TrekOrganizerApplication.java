13aac9d071ae7b3e6d71b8de838593f0088b859c
Your Name <you@example.com>
2021-03-01 10:45:19 +0200

    initial commit

---
+++ src/main/java/org/home/trekOrganizer/TrekOrganizerApplication.java
@@ -0,0 +1,13 @@
+package org.home.trekOrganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrekOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrekOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/home/trekOrganizer/TrekOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.home.trekOrganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrekOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
